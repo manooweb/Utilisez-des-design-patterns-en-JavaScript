a0cdee3bc2fb7a5cde2b6355ae668b20d130cece
Emmanuel Hesry <ehesry@gmail.com>
2026-06-18 11:01:22 +0200

    Implement pattern singleton user

--- js/db/User.js
+++ js/db/User.js
@@ -1,3 +1,24 @@
 class User {
-    
+     constructor(firstName, lastName) {
+        if (User.instance) {
+            return User.instance
+        }
+        
+        this._firstName = firstName
+        this._lastName = lastName
+
+         User.instance = this
+     }
+
+     get firstName() {
+         return this._firstName
+     }
+
+     get lastName() {
+         return this._lastName
+     }
+
+     get user() {
+         return { firstName: this._firstName, lastName: this._lastName }
+     }
 }
