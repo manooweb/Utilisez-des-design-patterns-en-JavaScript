2bc6125e024b72228dd05271074a91a4b84f11c5
Emmanuel Hesry <ehesry@gmail.com>
2026-06-18 16:48:08 +0200

    Partie 3 chapitre 2 pattern decorator

--- js/App.js
+++ js/App.js
@@ -2,7 +2,7 @@ class App {
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper')
         this.$modalWrapper = document.querySelector('.modal')
-        
+
         this.moviesApi = new MovieApi('/data/new-movie-data.json')
         this.externalMoviesApi = new MovieApi('/data/external-movie-data.json')
     }
@@ -24,7 +24,7 @@ class App {
         Filter.render()
 
         FullMovies.forEach(movie => {
-                const Template = new MovieCard(movie)
+                const Template = movieCardWithPlayer(new MovieCard(movie))
                 this.$moviesWrapper.appendChild(
                     Template.createMovieCard()
                 )
--- js/decorators/movieCardWithPlayer.js
+++ js/decorators/movieCardWithPlayer.js
@@ -1,2 +1,12 @@
 function movieCardWithPlayer(movieCard) {
+
+    if (movieCard.movie.actor === "sylvester") {
+        movieCard.$wrapper.addEventListener('click', () => {
+            console.log("click")
+            const Player = new PlayerModal(movieCard._movie)
+            Player.render()
+        });
+    }
+
+    return movieCard;
 }
