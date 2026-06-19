e50a35f459d4f587821a6d690c7905bd95041be6
Emmanuel Hesry <ehesry@gmail.com>
2026-06-19 15:38:34 +0200

    Partie 4 chapitre 3 template pattern

--- js/search/MainSearch.js
+++ js/search/MainSearch.js
@@ -1,7 +1,23 @@
-class Search {}
+class Search {
+    constructor(Movies) {
+        this.Movies = Movies
+    }
+
+    search(query){
+        return this.filterMovies(query)
+    }
+}
 
 
-class MovieNameSearch extends Search {}
+class MovieNameSearch extends Search {
+    filterMovies(query) {
+        return this.Movies.filter(Movie => Movie.title.toLowerCase().includes(query.toLowerCase()));
+    }
+}
 
 
-class ActorNameSearch extends Search {}
+class ActorNameSearch extends Search {
+    filterMovies(query) {
+        return this.Movies.filter(Movie => Movie.actor.toLowerCase().includes(query.toLowerCase()));
+    }
+}
--- js/templates/SearchForm.js
+++ js/templates/SearchForm.js
@@ -14,9 +14,8 @@ class SearchForm {
     search(query) {
         // Décomenter ces lignes de code une fois que votre recherche est fonctionnelle
 
-        /*
         let SearchedMovies = null
-        
+
         if (this.isSearchingByActor) {
             SearchedMovies = this.ActorNameSearch.search(query)
         } else {
@@ -24,7 +23,6 @@ class SearchForm {
         }
 
         this.displayMovies(SearchedMovies)
-        */
     }
 
     clearMoviesWrapper() {
@@ -66,7 +64,7 @@ class SearchForm {
         const searchForm = `
             <form action="#" method="POST">
                 <div class="search-input">
-                    <label for="search">Rechercher : </label> 
+                    <label for="search">Rechercher : </label>
                     <input id="search" type="text">
                 </div>
                 <div class="search-checkbox">
